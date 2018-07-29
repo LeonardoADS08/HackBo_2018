f95417df12c7081f00c36e88219e80ba945a88fe
Jorge Luis Gutierrez Fernandez <jorge.gutierrezf3@gmail.com>
2018-07-29 05:32:44 -0400

    inclusiones

---
+++ HackbotFB/DialogFlow/Models/RawResponse.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DialogFlow.Models
+{
+    public class Context
+    {
+        public string name { get; set; }
+        public Dictionary<string, string> parameters { get; set; }
+        public int lifespan { get; set; }
+    }
+
+    public class Metadata
+    {
+        public string intentId { get; set; }
+        public string webhookUsed { get; set; }
+        public string webhookForSlotFillingUsed { get; set; }
+        public string isFallbackIntent { get; set; }
+        public string intentName { get; set; }
+    }
+
+    public class Message
+    {
+        public int type { get; set; }
+        public string speech { get; set; }
+    }
+
+    public class Fulfillment
+    {
+        public string speech { get; set; }
+        public List<Message> messages { get; set; }
+    }
+
+    public class Result
+    {
+        public string source { get; set; }
+        public string resolvedQuery { get; set; }
+        public string action { get; set; }
+        public bool actionIncomplete { get; set; }
+        public Dictionary<string, string> parameters { get; set; }
+        public List<Context> contexts { get; set; }
+        public Metadata metadata { get; set; }
+        public Fulfillment fulfillment { get; set; }
+        public double score { get; set; }
+    }
+
+    public class Status
+    {
+        public int code { get; set; }
+        public string errorType { get; set; }
+    }
+
+    public class RawResponse
+    {
+        public string id { get; set; }
+        public DateTime timestamp { get; set; }
+        public string lang { get; set; }
+        public Result result { get; set; }
+        public Status status { get; set; }
+        public string sessionId { get; set; }
+    }
+}
