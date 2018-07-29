df0a2388763e8e7d1fd75849d3b3c0dad8276ed2
Jorge Luis Gutierrez Fernandez <jorge.gutierrezf3@gmail.com>
2018-07-29 03:27:27 -0400

    Connection

--- HackbotFB/HackbotFB/Controllers/FbConnectController.cs
+++ HackbotFB/HackbotFB/Controllers/FbConnectController.cs
@@ -48,10 +48,10 @@ namespace HackbotFB.Controllers
                         var msg = "You said: " + message.message.text;
                         var json = $@" {{recipient: {{  id: {message.sender.id}}},message: {{text: ""{msg + message.sender.id}"" }}}}";
                         Acciones.PostRaw("https://graph.facebook.com/v3.0/me/messages?access_token=EAAF3NCI0yUQBAMx3ZCirrlZCuYDNoLaD092M4ncaZAYmu03C5Rku5tCPFLZBqmh2LEjD03u6fZAw3NtLhJLO7WEiJuHZCOFSmbEiZAR1DsiZAZBEWdQ9qizdz0HDJCQeH1wZBhG4HVxKddyTtyKxaMBSZCnoXeSCJY4AARmf6C1wmyIOAZDZD", json);
-                        Task.Factory.StartNew(() =>
-                        {
-                            servicios.LlamarPost<dynamic>("http://localhost:61627/api/Message/Mensaje", message);
-                        });
+                        //Task.Factory.StartNew(() =>
+                        //{
+                        //    servicios.LlamarPost<dynamic>("http://localhost:61627/api/Message/Mensaje", message);
+                        //});
                     }
                 }
             });
